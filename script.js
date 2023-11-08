eaf490e5e9fea4fa83cad91e26c6dee95364193a
Percival Roberts <percival.roberts13@gmail.com>
2023-11-08 14:42:58 -0500

    inital commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// // Add this JavaScript code to trigger the animation when the page loads
+// document.addEventListener("DOMContentLoaded", function () {
+//     var container = document.querySelector(".container");
+//     container.classList.add("animate");
+// });
